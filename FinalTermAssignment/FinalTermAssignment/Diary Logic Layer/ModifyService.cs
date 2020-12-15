d45a2ff34e933de1cc058979e444899cc2494e1d
HabiburRahmanAnik <habib264521@gmail.com>
2020-12-16 01:41:36 +0600

    modify implement

---
+++ FinalTermAssignment/FinalTermAssignment/Diary Logic Layer/ModifyService.cs
@@ -0,0 +1,30 @@
+﻿using FinalTermAssignment.DataAcces;
+using FinalTermAssignment.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTermAssignment.Diary_Logic_Layer
+{
+    class ModifyService
+    {
+        ModifyEventDataAccess modifyEventDataAccess;
+        public ModifyService()
+        {
+            this.modifyEventDataAccess = new ModifyEventDataAccess();
+        }
+        public int ModifyNotes(int id,string note1,string date,string important)
+        {
+            Note note = new Note()
+            {
+                Id = id,
+                Note1 = note1,
+                Important = important,
+                Date = date
+            };
+            return this.modifyEventDataAccess.ModofyNotes(note);
+        }
+    }
+}
